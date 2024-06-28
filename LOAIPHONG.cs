275183e9148dc26ab49560a519badffff4007330
phantruon <nhattruongphan22112003@gmail.com>
2024-06-28 11:14:55 +0700

    Add files via upload

---
+++ LOAIPHONG.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace QUẢN_LÝ_KHÁCH_SẠN.Module.BusinessObjects.ORMDataModel1
+{
+    [DefaultClassOptions]
+    public partial class LOAIPHONG
+    {
+        public LOAIPHONG(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
